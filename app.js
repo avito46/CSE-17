8fe50e37b5dcbd59c2ba968c0cae1287a2c8786e
Aviral Dwivedi <aviraldwivedi410@gmail.com>
2026-04-05 13:15:25 +0530

    WEB-D

---
+++ app.js
@@ -0,0 +1,4 @@
+a=10
+b=5
+console.log("sum=",a+b)
+console.log("ans=",a+=5)
---
+++ index.js
@@ -0,0 +1,10 @@
+const student ={
+    name:"Aviral",
+    age:20,
+    cgpa:8.9,
+    ispass:true,
+
+};
+console.log(student.name)
+console.log(student.cgpa)
+console.log(student.ispass)
