4142515fbe864fefa405c8e038fd153fffacb66b
Aviral Dwivedi <aviraldwivedi410@gmail.com>
2026-05-27 19:58:13 +0530

    pattern

---
+++ main20.js
@@ -0,0 +1,34 @@
+let marks = 82;
+
+console.log("Grade Calculation Program");
+console.log("-------------------------");
+
+console.log("Student Marks = " + marks);
+
+if (marks >= 90) {
+
+    console.log("Grade = A+");
+
+} else if (marks >= 80) {
+
+    console.log("Grade = A");
+
+} else if (marks >= 70) {
+
+    console.log("Grade = B");
+
+} else if (marks >= 60) {
+
+    console.log("Grade = C");
+
+} else if (marks >= 50) {
+
+    console.log("Grade = D");
+
+} else {
+
+    console.log("Grade = F");
+}
+
+console.log("-------------------------");
+console.log("Program Completed");
