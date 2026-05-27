043be8f91c65d7ae4d536666fcc51947ffc93cad
Aviral Dwivedi <aviraldwivedi410@gmail.com>
2026-05-27 19:50:33 +0530

    pattern

---
+++ main11.js
@@ -0,0 +1,16 @@
+let arr = [5, 10, 15, 20, 25];
+
+let sum = 0;
+
+console.log("Array Elements:");
+
+for (let i = 0; i < arr.length; i++) {
+
+    console.log(arr[i]);
+
+    sum = sum + arr[i];
+}
+
+console.log("Total Sum of Array = " + sum);
+
+console.log("Program Completed");
